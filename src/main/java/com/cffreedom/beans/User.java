ce791622d9c02f63909e55f89166e0b509975996
markjacobsen <mark@markjacobsen.net>
2013-10-05 13:35:10 -0400

    Update to User

--- src/main/java/com/cffreedom/beans/User.java
+++ src/main/java/com/cffreedom/beans/User.java
@@ -34,10 +34,8 @@ public class User
 		this.email = email;
 	}
 	
-	public int getId()
-	{
-		return this.id;
-	}
+	public int getId() { return this.id; }
+	public void setId(int id) { this.id = id; }
 	
 	public String getUsername()
 	{
